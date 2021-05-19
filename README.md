b20642f9aac9b47f4bcc984bbe959a85ea0417f6
Jim Carroll <jim@pulamusic.com>
2021-05-19 08:18:27 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+> Jim Carroll |
+> 05/19/2021 |
+> [GitHub Profile](https://github.com/pulamusic)
+
+---
+
+# pulamusic-scripts
+
+This is a repo of utility scripts I often reuse.
+
+Using these scripts with [jsDeliver open-source CDN](https://www.jsdelivr.com/github).
---
+++ Switch_Statement_Example.js
@@ -0,0 +1,36 @@
+var answer = prompt("Pick a number from 1 to 10");
+
+switch(answer) {
+    case '1':
+        console.log("Well that was obvious.");
+        break;
+    case '2':
+        console.log("Just one step away from obvious.");
+        break;
+    case '3':
+        console.log("The polyrhythmic number.");
+        break;
+    case '4':
+        console.log("Quatro. Si.");
+        break;
+    case '5':
+        console.log("Cinco cervezas, por favor.");
+        break;
+    case '6':
+        console.log("Enh, six. OK.");
+        break;
+    case '7':
+        console.log("You know, I've never really bee a fan of seven. I don't know what it is, but the number seven creeps me out.");
+        break;
+    case '8':
+        console.log("ESPN8 - The OCHO.");
+        break;
+    case '9':
+        console.log("Almost there.... Must...make..it...further...");
+        break;
+    case '10':
+        console.log("Ten. Yes, that is a number.");
+        break;
+    default:
+        console.log("Wow, really? Let me say it slowly in a loud voice. CHOOSE EITHER 1, OR 2, OR 3, OR 4, OR 5, OR 6, OR 7, OR 8, OR 9, OR 10. It really is that simple.");
+}
